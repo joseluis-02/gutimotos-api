ae44a2e4dae02c4fed07d995d76aa12af14cf770
Jose Luis <joseluis.c.jlgl@gmail.com>
2025-10-10 17:02:19 -0400

    Cambio de tiempo de vida para access y refresh token

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -87,8 +87,8 @@ if firebase_key_json:
 
 # === JWT ===
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=10),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
